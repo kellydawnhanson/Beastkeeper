7815ef524095f9b543316ba234849d7c9267bd08
Kelly Hanson <kellyhanson@kellys-macbook-pro.local>
2018-05-24 09:21:25 -0600

    Set up CollectionViewCell - errors are from unimplemented collectionView methods Imported Magnificent Chicken images from desktop into Assets

---
+++ Beastkeeper/ProfileCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileCollectionViewCell.swift
+//  Beastkeeper
+//
+//  Created by Kelly Hanson on 2018-05-24.
+//  Copyright © 2018 Kelly Hanson. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    
+}
--- Beastkeeper/ProfileCollectionViewController.swift
+++ Beastkeeper/ProfileCollectionViewController.swift
@@ -10,25 +10,23 @@ import UIKit
 
 @IBDesignable //view UI elements in editor
 
-class ProfileCollectionViewController: UICollectionViewController {
+class ProfileCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    @IBOutlet weak var profileButtonStackView: UIStackView!
-
+    @IBOutlet var ProfileCollectionView: UICollectionView!
+    
     //create the "Magnificent Chicken" concept placeholder
     let MagnificentChicken: Classification = Classification (classificationType: "Magnificent Chicken")
     
     //make a chicken to test the initializers
-    lazy var chickenGeorge: Beast = Beast (classification: MagnificentChicken, name: "George", profilePicture: <#T##UIImage#>)
+    //lazy var chickenGeorge: Beast = Beast (classification: MagnificentChicken, name: "George", profilePicture: )
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //UICollectionView - needs to implement these methods
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
     }
-
 }
 
