454028f44f1664ed4198ea2de3ef661f58cc786f
Kelly Hanson <kellyhanson@kellys-macbook-pro.local>
2018-05-23 23:53:28 -0600

    testing recovery of lost commits

---
+++ Beastkeeper/DemoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beastkeeper
+//
+//  Created by Kelly Hanson on 2018-05-12.
+//  Copyright © 2018 Kelly Hanson. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable //view UI elements in editor
+
+class DemoViewController: UIViewController {
+    
+    @IBOutlet weak var profileButtonStackView: UIStackView!
+    
+    
+
+}
+
